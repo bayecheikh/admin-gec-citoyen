7213733bd3ee06bf5eb709e53ffe9e90edae1cde
diayacine <121964639+diayacine@users.noreply.github.com>
2023-06-20 17:25:33 +0000

    update

--- components/foireauxquestions/TableFoireAuxQuestions.vue
+++ components/foireauxquestions/TableFoireAuxQuestions.vue
@@ -119,13 +119,13 @@
                   >Modifier
                 </v-list-item-title>
               </v-list-item>
-              <!-- <v-list-item  @click="opendialog(item)" class="custom-v-list-action pl-2 pr-1" >
+              <v-list-item  @click="opendialog(item)" class="custom-v-list-action pl-2 pr-1" >
                 <v-list-item-title>
                   <v-icon small class="mr-2" v-bind="attrs" v-on="on">
                     mdi-delete-outline </v-icon
                   >Supprimer
                 </v-list-item-title>
-              </v-list-item> -->
+              </v-list-item>
             </v-item-group>
           </v-list>
         </v-menu>
